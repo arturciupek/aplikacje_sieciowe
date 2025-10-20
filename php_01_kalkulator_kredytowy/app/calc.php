2d2bd90e3dde8e58414383d3629eec625d11d993
arturciupek <arturtciupek18@gmail.com>
2025-10-20 19:45:17 +0200

    Etap 01: Kalkulator kredytowy

---
+++ php_01_kalkulator_kredytowy/app/calc.php
@@ -0,0 +1,70 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$kwota = $_REQUEST ['kwota'];
+$lata = $_REQUEST ['lata'];
+$procent = $_REQUEST ['procent'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($kwota) && isset($lata) && isset($procent))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $kwota == "") {
+	$messages [] = 'Nie podano kwoty kredytu';
+}
+if ( $lata == "") {
+	$messages [] = 'Nie podano liczby lat';
+}
+if ( $procent == "") {
+	$messages [] = 'Nie podano oprocentowania';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $kwota, $lata i $procent są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą';
+	}
+	
+	if (! is_numeric( $lata )) {
+		$messages [] = 'Druga wartość nie jest liczbą';
+	}	
+	if (! is_numeric( $procent )) {
+		$messages [] = 'Trzecia wartość nie jest liczbą ';
+	}	
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów
+	$kwota = intval($kwota);
+	$lata = intval($lata);
+	$procent = floatval($procent);
+	
+	//wykonanie operacji
+	$miesiecy = $lata * 12;
+    $kwota_z_odsetkami = $kwota * (1 + ($procent / 100) * $lata);
+    $rata = round($kwota_z_odsetkami / $miesiecy, 2);
+	
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$kwota,$lata,$procent,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
